98fc95d1ff21ee814feea0b96c7ea6ae270f9757
Rev0Lv <[colinhugopro@gmail.com]>
2023-05-03 16:45:29 +0200

    modif 2

--- assets/php/config.php
+++ assets/php/config.php
@@ -16,17 +16,29 @@ if ($connexion->connect_error) {
 echo "Connected successfully";
 
 // Insérer des lignes de couleurs différentes de sneakers en SQL
-$insertion1 = "INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Air Max 90', 'Bleu')";
-$insertion2 = "INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Jordan 1', 'Rouge')";
+
+$insertions = ["INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Air Max 90', 'Bleu')","INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Jordan 1', 'Rouge')"];
+
+foreach ($insertions as $insertion){
+    $result =$connexion->query($insertion);
+    if ($result == true) {
+        echo "Les lignes de couleurs différentes de sneakers ont été insérées avec succès";
+    } else {
+        echo "Erreur lors de l'insertion des lignes: " . $connexion->error;
+    }
+}
+
+/*$insertion2 = "INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Jordan 1', 'Rouge')";
 $insertion3 = "INSERT INTO chatbot_keywords (nom, couleur) VALUES ('Yeezy Boost 350', 'Noir')";
 
 if ($connexion->query($insertion1) === TRUE && $connexion->query($insertion2) === TRUE && $connexion->query($insertion3) === TRUE) {
     echo "Les lignes de couleurs différentes de sneakers ont été insérées avec succès";
 } else {
     echo "Erreur lors de l'insertion des lignes: " . $connexion->error;
-}
+}*/
 
 // Afficher le tableau de données SQL
+
 $requete = "SELECT * FROM chatbot_keywords";
 $resultat = $connexion->query($requete);
 
--- assets/sql/chatbot_keywords.sql
+++ assets/sql/chatbot_keywords.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 03 mai 2023 à 14:22
+-- Généré le : mer. 03 mai 2023 à 14:31
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.0.1
 
@@ -42,7 +42,28 @@ CREATE TABLE `chatbot_keywords` (
 --
 
 INSERT INTO `chatbot_keywords` (`chatbot_keywords_id`, `nom`, `taille`, `disponibilite`, `couleur`, `matiere`, `prix`) VALUES
-(1, '', '', '', '', '', '');
+(1, '', '', '', '', '', ''),
+(2, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(3, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(4, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(5, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(6, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(7, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(8, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(9, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(10, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(11, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(12, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(13, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(14, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(15, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(16, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(17, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(18, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(19, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL),
+(20, 'Air Max 90', NULL, NULL, 'Bleu', NULL, NULL),
+(21, 'Jordan 1', NULL, NULL, 'Rouge', NULL, NULL),
+(22, 'Yeezy Boost 350', NULL, NULL, 'Noir', NULL, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -62,7 +83,7 @@ ALTER TABLE `chatbot_keywords`
 -- AUTO_INCREMENT pour la table `chatbot_keywords`
 --
 ALTER TABLE `chatbot_keywords`
-  MODIFY `chatbot_keywords_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `chatbot_keywords_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
