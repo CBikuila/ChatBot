aa244f5e5890ab386827af248c5c4844a0078618
Bikuila <claver.bikuila@gmail.com>
2023-05-03 17:10:28 +0200

    modification

--- assets/php/deconnexion.php
+++ assets/php/deconnexion.php
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Déconnexion</title>
-    <link rel="stylesheet" href="../css/admin.css">
-    <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'>
-    <script defer src="https://kit.fontawesome.com/2812e639d2.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="titre-icone">
-      <i class="fas fa-user"></i>
-      <h1>Déconnexion</h1>
-    </div>
-    <form action="deconnexion.php" method="post">
-        <input type="submit" value="Se déconnecter">
-
-<?php
+<?
     session_start();
     session_destroy();
-    header("Location: dashboard.php");
-    exit;
+    header('location: ../dashboard.php');
 ?>
-
-    </form>
-</body>
-</html>
