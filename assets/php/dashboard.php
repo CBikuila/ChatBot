c3f32e666e436c74f92edf7da3488b2bb6b784ca
Bikuila <claver.bikuila@gmail.com>
2023-05-03 14:26:06 +0200

    dashboard modif

--- assets/php/dashboard.php
+++ assets/php/dashboard.php
@@ -1,7 +1,27 @@
-<?php
-//Code de PF connect.php dans boutique sur GDrive - 4. SUPPORTS COURS DEV - PHP
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
 
-
-
-print "dashbord";
-
+</head>
+<body>
+    <h1>Tableau de bord</h1>
+    <form action="" method="post">
+            <div class="preview text-center">
+                <span class="Error"></span>
+            </div>
+            <div class="form-group">
+                <label>Full Name:</label>
+                <input class="form-control" type="text" name="fullname" required placeholder="Enter Your Full Name"/>
+                <span class="Error"></span>
+            </div>
+            <div class="form-group">
+                <label>Email:</label>
+                <input class="form-control" type="email" name="email" required placeholder="Enter Your Email"/>
+                <span class="Error"></span>
+        </form>
+</body>
+</html>
