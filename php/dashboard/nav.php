36a80b294e32085091ab2e7ca27b7a9300413bbc
Rev0Lv <[colinhugopro@gmail.com]>
2023-05-04 16:54:07 +0200

    bouton deconnexion

--- php/dashboard/nav.php
+++ php/dashboard/nav.php
@@ -23,7 +23,7 @@
         <a href="dashboard.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Tableau de bord</a>
         <a href="mots_cles.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Mots-clés</a>
         <a href="produits.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Produits</a>
-        <a href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i>Déconnexion</a>
+        <a href="./../deconnexion.php" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i>Déconnexion</a>
     </nav>
 </div>
 
