a70bbc4e9cff00c678031a8be64f04b2d1cbec43
Bikuila <claver.bikuila@gmail.com>
2023-05-04 16:39:34 +0200

    amelioration 3

--- php/dashboard/nav.php
+++ php/dashboard/nav.php
@@ -22,7 +22,7 @@
     <nav class="dashboard-nav-list">
         <a href="dashboard.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Tableau de bord</a>
         <a href="mots_cles.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Mots-clés</a>
-        <a href="page_produit.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Produits</a>
+        <a href="produits.php" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Produits</a>
         <a href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i>Déconnexion</a>
     </nav>
 </div>
