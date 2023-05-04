0506145f371066b034fe017cae3b35b7bb3f9d7a
Bikuila <claver.bikuila@gmail.com>
2023-05-04 11:19:31 +0200

    suppréssion lien

--- php/dashboard.php
+++ php/dashboard.php
@@ -18,7 +18,7 @@
         <div class="dashboard-nav">
             <header>
                 <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
-                <a href="#" class="brand-logo"><img class="logo" src="../images/logo/sneakme-logo-clair.png"></a>              
+                <img class="logo" src="../images/logo/sneakme-logo-clair.png">             
             </header>
             <nav class="dashboard-nav-list">
             <a href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
