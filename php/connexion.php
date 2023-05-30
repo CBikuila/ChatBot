bff788aa98307ca2d5e8eab6eafb79658d77d0f7
CBikuila <claver.bikuila@gmail.com>
2023-05-30 11:57:59 +0200

    ajout div

--- php/connexion.php
+++ php/connexion.php
@@ -8,50 +8,51 @@
     <script defer src="https://kit.fontawesome.com/2812e639d2.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="titre-icone">
-      <i class="fas fa-user"></i>
-      <h1>Connexion</h1>
-    </div>
-    
-    <form action="connexion.php" method="post">
-        <label for="email">E-mail :</label>
-        <input type="text" id="email" name="email" placeholder="Enter votre email" required>
-        <br>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" placeholder="Mot de passe" required>
-        <br>
-        <input type="submit" value="Se connecter">
-        <?php
-            if ($_POST) {
-            $dashboard = [
-                [
-                "email" => "admin@sneakme.fr",
-                "password" => "admin",
-                ],
-            ];
-            
-            $email = $_POST["email"];
-            $password = $_POST["password"];
+    <div class="interface-connexion">
+        <div class="titre-icone">
+        <i class="fas fa-user"></i>
+        <h1>Connexion</h1>
+        </div>
+        <form action="connexion.php" method="post">
+            <label for="email">E-mail :</label>
+            <input type="text" id="email" name="email" placeholder="Enter votre email" required>
+            <br>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+            <br>
+            <input type="submit" value="Se connecter">
+            <?php
+                if ($_POST) {
+                $dashboard = [
+                    [
+                    "email" => "admin@sneakme.fr",
+                    "password" => "admin",
+                    ],
+                ];
+                
+                $email = $_POST["email"];
+                $password = $_POST["password"];
 
-            $loggedIn = false;
+                $loggedIn = false;
 
-                foreach ($dashboard as $user) {
-                    if ($user["email"] == $email && $user["password"] == $password) {
-                        $loggedIn = true;
-                        break;
+                    foreach ($dashboard as $user) {
+                        if ($user["email"] == $email && $user["password"] == $password) {
+                            $loggedIn = true;
+                            break;
+                        }
+                    }
+
+                    if ($loggedIn === false && $email && $password) {
+                        echo "Adresse mail et/ou mot de passe incorrect";
+
+                        // L'utilisateur est connecté, faire quelque chose ici...
+
+                    } else {
+                        header('Location: ../php/dashboard/dashboard.php');
                     }
                 }
-
-                if ($loggedIn === false && $email && $password) {
-                    echo "Adresse mail et/ou mot de passe incorrect";
-
-                    // L'utilisateur est connecté, faire quelque chose ici...
-
-                } else {
-                    header('Location: ../php/dashboard/dashboard.php');
-                }
-            }
-        ?>    
-    </form>
+            ?>    
+        </form>
+    </div>
 </body>
 </html>
