f1c500a3d9d6f8b84eaa9eb2eea2ec3d54dfd48b
Rev0Lv <[colinhugopro@gmail.com]>
2023-05-31 17:11:21 +0200

    LISTE DEROULANTE STATUT COMMANDES

--- php/dashboard/dashboard.php
+++ php/dashboard/dashboard.php
@@ -1,22 +1,22 @@
-<?php 
-    require('nav.php');
+<?php
+require('nav.php');
 ?>
-    <div class='dashboard'>  
-        <div class='dashboard-app'>
-            <div class='dashboard-content'>
-                <div class='container'>
-                    <div class='card'>
-                        <div class='card-header'>
-                            <h1>Salut Boss !</h1>
-                        </div>
-                        <div class='card-body'>
-                            <p>Bienvenue dans ton compte administrateur.</p>
-                        </div>
+<div class='dashboard'>
+    <div class='dashboard-app'>
+        <div class='dashboard-content'>
+            <div class='container'>
+                <div class='card'>
+                    <div class='card-header'>
+                        <h1>Salut Boss !</h1>
+                    </div>
+                    <div class='card-body'>
+                        <p>Bienvenue dans ton compte administrateur.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-<?php 
-    require('footer.php');
+</div>
+<?php
+require('footer.php');
 ?>
