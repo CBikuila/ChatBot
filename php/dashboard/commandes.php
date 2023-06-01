5327044aab6e7e6190922afd255f729b6c880737
Rev0Lv <[colinhugopro@gmail.com]>
2023-06-01 13:48:06 +0200

    Jointure de la table SQL produits avec la table statuts de commandes et création de liste déroulante de statut produit

--- php/dashboard/commandes.php
+++ php/dashboard/commandes.php
@@ -5,28 +5,41 @@
 </head>
 <body>
     <table>
-            <tr>
-                <th>Marque</th>
-                <th>Modèle</th>
-                <th>Couleur</th>
-                <th>Taille</th>
-                <th>Prix</th>
-            </tr> 
+        <tr>
+            <th>Marque</th>
+            <th>Modèle</th>
+            <th>Couleur</th>
+            <th>Taille</th>
+            <th>Prix</th>
+            <th>Statut de commande</th>
+        </tr> 
+
         <?php
-            // Connexion à la base de données
-            $conn = new mysqli("localhost", "root", "root", "sneakme_database");
+        require('../config.php');
+        include('nav.php');
 
-            // Vérification de la connexion
-            if ($conn->connect_error) {
+        // Connexion à la base de données
+        $conn = new mysqli("localhost", "root", "root", "sneakme_database");
+
+        // Vérification de la connexion
+        if ($conn->connect_error) {
             die("Erreur de connexion à la base de données : " . $conn->connect_error);
-            }
+        }
 
-            // Exécution de la requête SQL
-            $sql = "SELECT produits_id, marque_sneakers, modele_sneakers, couleur_sneakers, taille_sneakers, prix_sneakers FROM produits";
-            $result = $conn->query($sql);
+        // Exécution de la requête SQL pour récupérer les produits et leur statut de commande
+        $sql = "SELECT p.produits_id, p.marque_sneakers, p.modele_sneakers, p.couleur_sneakers, p.taille_sneakers, p.prix_sneakers, s.statuts_commandes_etapes
+                FROM produits p
+                LEFT JOIN statuts_commandes s ON p.produits_id = s.commandes_id";
 
-            // Vérification des résultats de la requête
-            if ($result->num_rows > 0) {
+        $result = $conn->query($sql);
+
+        // Vérification de la réussite de la requête
+        if (!$result) { //le "!" signifie : "si je n'arrive pas à ouvrir le fichier, alors..."
+            die("Erreur lors de l'exécution de la requête : " . $conn->error);
+        }
+
+        // Vérification des résultats de la requête
+        if ($result->num_rows > 0) {
             // Affichage des lignes
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
@@ -35,67 +48,38 @@
                 echo "<td>" . $row["couleur_sneakers"] . "</td>";
                 echo "<td>" . $row["taille_sneakers"] . "</td>";
                 echo "<td>" . $row["prix_sneakers"] . "</td>";
-                echo '<td><a class="btn btn-danger btn-xs" href="../suppressionLigneSQL.php?id=' . $row["produits_id"] . ' ">Supprimer</a></td>';
+                echo "<td>";
+                echo "<select>";
+
+                // Requête SQL pour récupérer les statuts de commande
+                $statuts_sql = "SELECT statuts_commandes_etapes FROM statuts_commandes";
+                $statuts_result = $conn->query($statuts_sql);
+
+                // Vérification de la réussite de la requête
+                if ($statuts_result->num_rows > 0) {
+                    while ($statut_row = $statuts_result->fetch_assoc()) {
+                        echo "<option>" . $statut_row["statuts_commandes_etapes"] . "</option>";
+                    }
+                } else {
+                    echo "<option>Aucun statut de commande trouvé</option>";
+                }
+
+                echo "</select>";
+                echo "</td>";
                 echo "</tr>";
             }
-            } else {
-            echo "<tr><td colspan='3'>Aucuns produits ajoutés</td></tr>";
-            }
+        } else {
+            echo "<tr><td>Aucun produit ajouté</td></tr>";
+        }
 
-            // Fermeture de la connexion à la base de données
-            $conn->close();
+        // Fermeture de la connexion à la base de données
+        $conn->close();
         ?>
+
     </table>
 </body>
 </html>
  
-<?php //CONNEXION A LA TABLE SQL "statuts_commandes_etapes" ET AFFICHAGE SON FORME DE MENU DEROULANT
-    require('../config.php');
-    include('nav.php');
-    $conn = new mysqli("localhost", "root", "root", "sneakme_database");
-
-// Vérification de la connexion
-if ($conn->connect_error) {
-die("Erreur de connexion à la base de données : " . $conn->connect_error);
-}
-
-
-// Exécution de la requête SQL
-$sql = "SELECT statuts_commandes_etapes FROM statuts_commandes";
-$result = $conn->query($sql);
-
-// Générer la liste déroulante avec les statuts de commande
-echo '
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Menu déroulant statut de commande</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-</head>
-<body>
-    <div class="btn-group">
-        <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Statut de commande</button>
-        <ul class="dropdown-menu">
-';
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo '<li><a class="dropdown-item" href="#">' . $row["statuts_commandes_etapes"] . '</a></li>';
-    }
-} else {
-    echo '<li><a class="dropdown-item" href="#">Aucun statut de commande trouvé</a></li>';
-}
-
-echo '
-        </ul>
-    </div>
-</body>
-</html>
-';
-
-// Fermer la connexion à la base de données
-$conn->close();
-
-require('footer.php');
+<?php
+    require('footer.php');
 ?>
