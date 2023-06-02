0a3470c3ea3a4a3ded441ff8e9348a57891defbc
Rev0Lv <[colinhugopro@gmail.com]>
2023-06-02 09:31:21 +0200

    changement localisation images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const chatButton = document.querySelector('.chatbox__button');
 const chatContent = document.querySelector('.chatbox__support');
 const icons = {
-    isClicked: '<img src="./images/icons/bulledark.png" />',
-    isNotClicked: '<img src="./images/icons/bulledark.png" />'
+    isClicked: '<img src="assets/images/icons/bulledark.png" />',
+    isNotClicked: '<img src="assets/images/icons/bulledark.png" />'
 }
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
