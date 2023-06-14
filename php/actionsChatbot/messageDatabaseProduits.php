ab8c20b214bfe2ea08695f2082c5d2f06d4b8d03
[REv0Lv] <[colinhugopro@gmail.com]>
2023-06-14 14:47:04 +0200

    modifs

--- php/actionsChatbot/messageDatabaseProduits.php
+++ php/actionsChatbot/messageDatabaseProduits.php
@@ -8,17 +8,14 @@ $_POST = json_decode(array_keys($_POST)[0], 1);
 $commande = $_POST['produits'];
 
 // Requête pour récupérer la question associée au mot-clé
-$requete = "SELECT marque_sneakers, modele_sneakers, couleur_sneakers, taille_sneakers, genre_sneakers, prix_sneakers FROM produits WHERE produits = \"$commande\""; 
+$requete = "SELECT marque_sneakers, modele_sneakers, couleur_sneakers, taille_sneakers, genre_sneakers, prix_sneakers FROM produits"; 
 $resultat = $conn->query($requete);
-
 if ($resultat->num_rows > 0) {
     // Récupérer la première ligne de résultat
-    $ligne = $resultat->fetch_assoc();
-    $questionAssociee = $ligne['produits'];
-
+    $produits = $resultat->fetch_all(MYSQLI_ASSOC);
     // Renvoyer la listes des produits au format JSON
-    $reponse = array('question' => $questionAssociee);
-    echo json_encode($reponse);
+    
+    echo json_encode($produits);
 } else {
     // Si aucun résultat n'est trouvé, renvoyer une réponse vide au format JSON
     $reponse = array('question' => '');
